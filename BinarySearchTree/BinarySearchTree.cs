d866dc0424a6129ecd77b9c31e4c139d7111b362
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-11 14:34:46 -0600

    Init Commit. Created Node class and BinarySearchTree class.

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        //MM
+        public Node root;
+
+        //CTOR
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        //MM
+        
+        public void AddNode(int data)
+        {
+            //see if there is a root node
+            //if no root node, set new node as root node
+            Node newNode = new Node(data);
+            newNode.data = data;
+            if(root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node currentNode = root;
+                //create parent???
+                Node parentNode;
+                while (true)
+                {
+                    parentNode = currentNode;
+                    if(data < currentNode.data)
+                    {
+                        currentNode = currentNode.leftChild;
+                        if(currentNode = null)
+                        {
+                            parentNode.leftChild = newNode;
+                        }
+                    }
+                }
+            }
+
+            //create parent???
+            //determine if > root node or < root node
+            //if > root node create leftChild
+            //if < root node create rightChild
+            //
+        }
+
+        //public void RemoveNode()
+        //{
+        //    //logic
+        //}
+
+        public void Search(int x)
+        {
+            //logic
+        }
+
+        public void Menu()
+        {
+            //be able to choose to AddNode or Search
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        //MV
+        public int data;
+        public Node leftChild;
+        public Node rightChild;
+        
+
+        //CTOR
+        public Node(int data)
+        {
+            this.data = data;
+            edge = null;
+
+        }
+
+        //MM
+    }
+}
